cdde8727ec8cdc85735aba0c606af66c2530af72
Diederik de Groot <ddegroot@talon.nl>
2018-01-15 15:11:42 +0100

    Fix druntime unittest clone

--- scripts/master.mk
+++ scripts/master.mk
@@ -19,9 +19,9 @@ all: master_dmd.tar.bz2
 clone_master:
 	[ -d master ] || mkdir master
 	$(GIT) -C master clone -b dragonflybsd-master https://github.com/${GITUSER}/dmd.git
-	$(GIT) -C master clone https://github.com/${GITUSER}/druntime.git
+	$(GIT) -C master clone -b dragonflybsd-master https://github.com/${GITUSER}/druntime.git
 	cd master/druntime; $(GIT) checkout -b unittest;
-	cd master/druntime; $(GIT) pull origin dragonflybsd-master dragonfly-core.sys.posix dragonfly-core.sys.dragonflybsd --commit -q --squash;
+	cd master/druntime; $(GIT) pull origin dragonfly-core.sys.posix dragonfly-core.sys.dragonflybsd --commit -q --squash;
 	$(GIT) -C master clone https://github.com/dlang/phobos.git
 	touch $@
 	
